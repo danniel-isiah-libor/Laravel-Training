92e55a98e19d3bcd1e67c8dce1b1ec515e4b9d1f
kimdkt <k.patricio@dkt.com.ph>
2024-11-08 18:35:41 +0800

    ROUTES, Controller, Model Introduction

--- laravel002/app/Http/Controllers/UserController.php
+++ laravel002/app/Http/Controllers/UserController.php
@@ -6,5 +6,51 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * Search 
+     * 
+     * @return string
+     */
+    public function search() {
+        return 'User Search';
+    }
+
+    /**
+     * Store 
+     * 
+     * @return string
+     */
+    public function store() {
+        return 'User store';
+    }
+
+    /**
+     * 
+     * 
+     */
+    public function show(Request $request, $id = null){
+
+     dd($request->all());
+
+     return "User ID: $id";
+
+    //    $profileModel = new Profile();
+
+    //   return $profileModel->getProfile();
+
+    }
+
+
+    /**
+     * Store 
+     * 
+     * @return string
+     */
+    public function register(Request $request){
+        $parameters = $request->boolean('is_active', false);
+
+        $request->merge(['user_id' => 1]);
+    
+        dd($request->all());
+    }
 }
--- laravel002/routes/web.php
+++ laravel002/routes/web.php
@@ -1,59 +1,16 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::view('/', 'welcome');
-
-
-// Route::get('/', function () {
-// //  return "<script>alert('hello')</script>";
-// //  return "<h1 style='color:red'>Hello World</h1>";
-//     return (object) [1, 2, 3];
-// });
-
 Route::name('users.')->prefix('/users')->group(function () {
-    Route::get('/search', function () {
-        return 'User Search';
-    })->name('search');
-
-    Route::get('/store', function () {
-        return 'User Store';
-    })->name('store');
+    Route::get('/search', [UserController::class, 'search'])->name('search');
+    Route::get('/store', [UserController::class, 'store'])->name('store');
 });
-
-// Route::get('/redirect', function () {
-//     return redirect()->route('users.search');
-// });
-
-// Route::redirect('/from', '/to');
-
-// Route::fallback(function () {
-//     return "NOT FOUND";
-// });
-
-// Route::get('/users/{id}', function ($id) {
-//     return "User ID: $id";
-// });
 
 Route::prefix('/users')->group(function () {
-    Route::get('{id?}', function ($id = null) {
-        return "User ID: $id";
-    });
+    Route::get('/show/{id?}', [UserController::class, 'show'])->name('show');
 });
 
-// Route::get('/register', function (Request $request) {
-//     dd($request->query('name', 'James'));
-// });
-
-Route::get('/register', function (Request $request) {
-    $parameters = $request->boolean('is_active', false);
-
-    $request->merge(['user_id' => 1]);
-
-    dd($request->all());
-});
+Route::get('/register', [UserController::class, 'register'])->name('users.register');
