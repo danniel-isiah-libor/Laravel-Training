304e261c7ac9baf2d0d51759e997c57c40e284c2
jaylusabia <jaylusabia@gmail.com>
2024-11-08 15:49:21 +0800

    first day 11/8

---
+++ jayl-app/routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+//Route::get('/', function () {
+//   return view('welcome');
+//});
+
+//Route::get('/',function(){
+//  return [1,2,3];
+//});
+
+Route::prefix('/users')->name('users.')->group(function () {
+    Route::get('/search', function () {
+        // Matches The "/admin/users" URL
+        return 'user search';
+    })->name('search');
+
+    Route::get('/store', function () {
+        return 'User Store';
+    })->name('store');
+
+    Route::get('{id}', function ($id = null) {
+        return "user ID: $id";
+    });
+
+    Route::get('/',function(){
+        return 'index user';
+    });
+});
+
+Route::get('/redirect', function () {
+    return redirect()->route('users.search');
+});
+
+Route::redirect('/from', ('/to'));
+
+// Route::fallback(function () {
+//     return 'NOT FOUND';
+// });
