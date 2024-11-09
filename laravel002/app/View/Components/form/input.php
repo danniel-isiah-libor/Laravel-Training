f22c978e83f592881d38c01898a8fc6c3133eef8
kimdkt <k.patricio@dkt.com.ph>
2024-11-09 11:06:51 +0800

    Day 2 - Introduction for Views or Front End

---
+++ laravel002/app/View/Components/form/input.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\View\Components\form;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class input extends Component
+{
+
+    private $label;
+    private $type;
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct($label, $type= 'text')
+    {
+        $this->label = $label;
+        $this->type = $type;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.form.input', [
+           'label' => $this->label,
+           'type' => $this->type
+        ]);
+    }
+}
