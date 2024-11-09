0b3ddd5a525d062acdc33c8884ee05ef82b2c880
ezekielsans <jhonmartolentino021@gmail.com>
2024-11-09 10:34:09 +0800

    ok

---
+++ example-app/resources/views/components/form/input.blade.php
@@ -0,0 +1,6 @@
+{{-- <div>
+    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
+</div> --}}
+
+<label for="name" class="form-label">Name</label>
+<input type="text" class="form-control">
