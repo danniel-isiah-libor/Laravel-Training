7766e95bb25327fd546874d6428effbee30ff9f0
danniel-isiah-libor <dlibor.dev@gmail.com>
2024-11-10 15:31:51 +0800

    [Day 3] seeder

--- playground/routes/console.php
+++ playground/routes/console.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Laravel\Telescope\Telescope;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// Schedule::command('telescope:prune')->daily();
+// Schedule::command('telescope:clear')->daily();
