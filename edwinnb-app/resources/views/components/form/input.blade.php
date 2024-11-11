d9262fc4cffd471670fb827cc96216f3d5e811c4
edwinnb <edwin_n_b@yahoo.com>
2024-11-11 13:23:35 +0800

    Day 4 - Pass value

--- edwinnb-app/resources/views/components/form/input.blade.php
+++ edwinnb-app/resources/views/components/form/input.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group">
-    @props(['name'])
+    @props(['name'],'value'=null)
     <label class="form-label">{{ $label }}</label>
-    <input type="{{ $type }}" name="{{ $name }}"  style="@error($name) border: 1px solid red @enderror" class="form-control {{ $class }}" value="{{ old($name)}}">
+    <input type="{{ $type }}" name="{{ $name }}"  style="@error($name) border: 1px solid red @enderror" class="form-control {{ $class }}" value="{{ old($name)}}??value">
     @error($name)
         <p style="color:red">{{ $message }}</p>
     @enderror
